e88eab6a3ed44b1862f458d3b847ecb59b051c01
Nikhil Dev <nikhil@nikhildev.com>
2015-02-02 11:03:53 +0530

    Duplicated factory and controller on /weddings. Need to change later

--- components/weddings/weddings.js
+++ components/weddings/weddings.js
@@ -15,6 +15,47 @@ angular.module('components.weddings', [])
 
 })
 
-.controller('WeddingsController', ['$scope', function($scope) {
-  console.log('WeddingsController');
-}]);
+.controller('WeddingsController', WeddingsController)
+.factory('WeddingsFactory', WeddingsFactory);
+
+function WeddingsController($scope, WeddingsFactory, Utils, ImageBoxFactory) {
+  var vm = this;
+
+  WeddingsFactory.getRandomImages()
+  .then(
+    function(response) {
+      vm.randomImages = Utils.getRandom(response.photo, 24);
+    }, function(error) {
+      console.log(error);
+    });
+
+  vm.openImage = function(imageObject) {
+    ImageBoxFactory.showImage(imageObject);
+  };
+
+}
+
+function WeddingsFactory($http, $q, FLICKR_API_URL, EXTRAS_PARAMS) {
+  console.log(FLICKR_API_URL);
+
+  var methodString = '&method=flickr.people.getPublicPhotos';
+  var weddingsFactory = {};
+
+  weddingsFactory.getRandomImages = function() {
+    return $http.get(FLICKR_API_URL + methodString,
+    {
+      params: {
+        per_page: 10000,
+        extras: EXTRAS_PARAMS
+      }
+    })
+    .then(function(response) {
+      weddingsFactory.randomImages = response.data.photos;
+      return weddingsFactory.randomImages;
+    }, function(error) {
+      return $q.reject(error);
+    });
+  }
+
+  return weddingsFactory;
+}
